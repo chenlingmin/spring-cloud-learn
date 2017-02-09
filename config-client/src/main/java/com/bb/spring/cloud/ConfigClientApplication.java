bd2be6f4348917b0fe0fb052ae9a0b8edd0ddde8
陈灵敏 <chenlingmin@zhongzijinfu.com>
2017-02-09 15:03:05 +0800

    config client add eureka and actuator

--- config-client/src/main/java/com/bb/spring/cloud/ConfigClientApplication.java
+++ config-client/src/main/java/com/bb/spring/cloud/ConfigClientApplication.java
@@ -2,10 +2,12 @@ package com.bb.spring.cloud;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Created by Chenlm on 09/02/2017.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ConfigClientApplication {
     public static void main(String[] args) {
